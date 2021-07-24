e04d41e9c79b191773350435e999ad3ebdd4ce0b
arensusu <willy9966@gmail.com>
2021-07-24 09:52:22 +0800

    0724

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    num = event.source
+    num = event.source.userId
     message = TextSendMessage(text=num)
     line_bot_api.reply_message(event.reply_token, message)
 
