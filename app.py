a2bc80454a9065b11a03b1fda014e599c98dd54b
arensusu <willy9966@gmail.com>
2021-09-01 08:46:10 +0800

    0901

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import os
 import psycopg2
-
+"""
 DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode = "require")
@@ -9,7 +9,7 @@ cursor = conn.cursor()
 
 cursor.close()
 conn.close()
-
+"""
 from flask import Flask, request, abort
 
 from linebot import (
@@ -33,7 +33,14 @@ accountAction = ["群組帳戶", "個人帳戶"]
 paymentAction = ["收入", "支出"]
 listAction = ["本週明細", "本月明細"]
 
+#mention list
 mainMention = ["選擇項目(群組帳戶, 個人帳戶)", "選擇項目(收入, 支出)", "選擇項目(本週明細, 本月明細)"]
+actionMention = ["請輸入帳戶名稱"]
+paymentMention = ["請選擇帳戶", "請輸入品項及金額(ex: 早餐 50)"]
+
+#chat history
+main = -1
+side = -1
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -53,6 +60,28 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    #choose action
+    if main == -1 and side == -1 :
+        for i in range(len(mainAction)):
+            if event.message.text == mainAction[i]:
+                main = i
+                """record to chat history"""
+                message = TextSendMessage(text = mainMention[i])
+                break
+            else:
+                message = TextSendMessage(text = "error")
+        
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+    elif main != -1 and side == -1 :
+        action(event, main)
+    elif main != -1 and side != -1 :
+        action(event, main, side)
+    else :
+        action()
+
+    """
     if event.message.text == mainAction[0] :
         message = TextSendMessage(text = mainMention[0])
     elif event.message.text == mainAction[1] : 
@@ -63,7 +92,7 @@ def handle_message(event):
         message = TextSendMessage(text="error")
         
     line_bot_api.reply_message(event.reply_token, message)
-    """
+    """"""
     num = event.source.user_id
     message = TextSendMessage(text=num)
     line_bot_api.reply_message(event.reply_token, message)
